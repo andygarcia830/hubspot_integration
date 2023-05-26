d511d5399a1dfb0f96cfc0d2eb28cf770124f476
Andy Garcia <andy@xurpas.com>
2023-05-26 16:25:39 +0800

    Added auto daily sync

--- hubspot_integration/tasks.py
+++ hubspot_integration/tasks.py
@@ -1,5 +1,15 @@
 import  hubspot_integration.hubspot_integration.doctype.hubspot_configuration.hubspot_configuration
 
+
+def all():
+    pass
+
 def daily():
     hubspot_integration.hubspot_integration.doctype.hubspot_configuration.hubspot_configuration.fetch_won_deals()
     hubspot_integration.hubspot_integration.doctype.hubspot_configuration.hubspot_configuration.fetch_companies()
+
+def weekly():
+    pass
+
+def monthly():
+    pass
