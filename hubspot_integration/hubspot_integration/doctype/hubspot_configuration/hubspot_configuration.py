c595c3cf616f522eed204928543692a5af1a9b05
Andy Garcia <andy@xurpas.com>
2023-06-05 17:16:37 +0800

    fixed dealstage retrieval

--- hubspot_integration/hubspot_integration/doctype/hubspot_configuration/hubspot_configuration.py
+++ hubspot_integration/hubspot_integration/doctype/hubspot_configuration/hubspot_configuration.py
@@ -17,77 +17,80 @@ def fetch_won_deals():
 	doc = frappe.get_doc('Hubspot Configuration')
 	dealstage = doc.deal_stage_search_value
 	body = {}
-	filtergroup=[]
-	filters={
-      "filters": [
-        {
-          "propertyName": "dealstage",
-          "operator": "EQ",
-          "value": dealstage
-        }
-      ]
-    }
-
-	filtergroup.append(filters)
-
-	body['filterGroups']=filtergroup
-
-	print(body)
-
-	endpoint = doc.deals_api_endpoint
-	access_token = doc.access_token
-	headers={'Authorization':'Bearer '+access_token,'Content-Type':'application/JSON'}
 	
-	hasMore = True
-	after = 0;
-	total = 0;
-	ctr = 0
-
-	while hasMore:
-		body['after']=after
-		jsonStr = json.dumps(body)
-		print(jsonStr)
-		x = requests.post(endpoint, headers=headers, data = jsonStr)
-		print(x.text)
-		resp = json.loads(x.text)
-		total = resp['total']
-		
-		print(f'TOTAL={total}')
-		
-		results = resp['results']
-		for item in results:
-			ctr = ctr+1
-			thisdoc = frappe.new_doc('Deal')
-			thisdoc.deal_id=item['id']
-			thisdoc.status='NEW'
-			try:
-				thisdoc = frappe.get_doc('Deal',item['id'])
-			except:
-				pass
-			properties = item['properties']
-			thisdoc.name1=properties['dealname']
-			thisdoc.amount=properties['amount']
-			try:
-				thisdoc.create_date=properties['createdate'][0:properties['createdate'].index('T'):1]
-			except:
-				pass
-			try:
-				thisdoc.close_date=properties['closedate'][0:properties['closedate'].index('T'):1]
-			except:
-				pass
-
-			try:
-				thisdoc.save()
-			except:
-				pass
 	
-		try:						# FINISH IF NO MORE PAGING
-			page = resp['paging']
-			next = page['next']
-			after = next['after']
-			print(f'PAGE={after}')
-		except:
-			hasMore = False
+	for item in dealstage.split(','):
+		filtergroup=[]
+		filters={
+		"filters": [
+			{
+			"propertyName": "dealstage",
+			"operator": "EQ",
+			"value": item
+			}
+		]
+		}
+
+		filtergroup.append(filters)
+
+		body['filterGroups']=filtergroup
+
+		print(body)
+
+		endpoint = doc.deals_api_endpoint
+		access_token = doc.access_token
+		headers={'Authorization':'Bearer '+access_token,'Content-Type':'application/JSON'}
+		
+		hasMore = True
+		after = 0;
+		total = 0;
+		ctr = 0
+
+		while hasMore:
+			body['after']=after
+			jsonStr = json.dumps(body)
+			print(jsonStr)
+			x = requests.post(endpoint, headers=headers, data = jsonStr)
+			print(x.text)
+			resp = json.loads(x.text)
+			total = resp['total']
+			
+			print(f'TOTAL={total}')
+			
+			results = resp['results']
+			for item in results:
+				ctr = ctr+1
+				thisdoc = frappe.new_doc('Deal')
+				thisdoc.deal_id=item['id']
+				thisdoc.status='NEW'
+				try:
+					thisdoc = frappe.get_doc('Deal',item['id'])
+				except:
+					pass
+				properties = item['properties']
+				thisdoc.name1=properties['dealname']
+				thisdoc.amount=properties['amount']
+				try:
+					thisdoc.create_date=properties['createdate'][0:properties['createdate'].index('T'):1]
+				except:
+					pass
+				try:
+					thisdoc.close_date=properties['closedate'][0:properties['closedate'].index('T'):1]
+				except:
+					pass
+
+				try:
+					thisdoc.save()
+				except:
+					pass
+		
+			try:						# FINISH IF NO MORE PAGING
+				page = resp['paging']
+				next = page['next']
+				after = next['after']
+				print(f'PAGE={after}')
+			except:
+				hasMore = False
 
 
 
